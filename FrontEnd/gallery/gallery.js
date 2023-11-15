3db9efb1d2bee2539c12885b6db64393d1bd7d2b
Axel Gregoire <professionnel.agregoire@gmail.com>
2023-11-15 16:09:46 +0100

    Avancement

--- FrontEnd/gallery/gallery.js
+++ FrontEnd/gallery/gallery.js
@@ -1,11 +1,3 @@
-// Element Dom sur la Div Gallery
-// const ImagesGallery = document.getElementsByClassName("gallery") async () => {
-//     var fetchResult = await fetch('http://localhost:5678/api/works')
-//     var data = await fetchResult.json()
-//     for ()
-// }
-
-
 function Test() {
   const Gallery = document.querySelector(".gallery")
 const url = 'http://localhost:5678/api/works';
@@ -14,41 +6,28 @@ fetch(url)
   .then(response => response.json())
   .then(datas => {
     for( let data of datas) {
-      const p = document.createElement("p");
-      p.innerHTML = data.title;
-      // let display = ""
-      // display += `<p>${data.title}</p>`
-      Gallery.appendChild(p)
-      // const imgGallery = data.imageUrl;
+      const figure = document.createElement("figure")
+      
+    // Insertion des images de l'api  
+      let dynamiqueimage = document.createElement("img")
+      dynamiqueimage.src = data.imageUrl;
+      dynamiqueimage.alt = data.name;
+
+    // Insertion des titres des images   
+      const figcaption = document.createElement("figcaption");
+      figcaption.innerHTML = data.title;
+    
+    // construction des balises  
+      figure.appendChild(dynamiqueimage)
+      figure.appendChild(figcaption)
+      Gallery.appendChild(figure)
+    
     }
   })
 
 }
 
  Test();
-
-//   async function ajoutListenerGallery() {
-//      await fetch ('http://localhost:5678/api/works')        
-
-
-// }
-
-// const ImagesGallery = fetch ('http:"//localhost:5678/api/works')
-//     for ( let i = 1; i < ImagesGallery.length ; i++ ) {
-//         console.log(id)
-//     }  
-
-// //        // creation balise pour la gallery general
-// const sectionGallery = document.createElement("article");
-// // creation des balises ( img - titre de l'image etc ...)
-// const ImgGallery = document.createElement("img");
-
-// const DescriptionImg = document.createElement(p)
-// DescriptionImg.innerText = article.description ?? "Pas de descritpion pour le moment"
-
-// // rattacher la balise a la section
-// ImgGallery.appendChild(sectionGallery)
-// DescriptionImg.appendChild(ImgGallery)
 
 
 
