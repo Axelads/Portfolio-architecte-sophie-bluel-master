d28f599dd8430b6fb4be96af9587d4fb368f0ab2
Axel Gregoire <professionnel.agregoire@gmail.com>
2023-11-21 12:17:08 +0100

    Remise en forme avec des boutons pour le Filtre en Dynamique(manque le bouton TOUS)

--- FrontEnd/Filtre/filtre.js
+++ FrontEnd/Filtre/filtre.js
@@ -1,85 +1,24 @@
-// Tableau de filtres
-const BtnFiltre = ["Tous", "Objets", "Appartements", "Hôtels & restaurants"];
+const formulaire = document.querySelector(".btnS");
 
-// Fonction pour créer les boutons et les ajouter au formulaire
-function creerBoutons() {
-  // Sélectionnez le formulaire avec la classe "btnS"
-  const formulaire = document.querySelector(".btnS");
+function AjoutBouton () {
 
-  if (formulaire) {
-    for (let i = 0; i < BtnFiltre.length; i++) {
-      // Créez un bouton pour chaque élément du tableau
-      const bouton = document.createElement("button");
-      bouton.textContent = BtnFiltre[i];
-
-
-
-  // Appel a l'api Catégorie pour trouver les "name"
+//api /categories  
   const UrlCategorie = 'http://localhost:5678/api/categories';
-    fetch(UrlCategorie)
+  
+ // Appel de mon API 
+  fetch(UrlCategorie)
     .then(reponse => reponse.json())
-    .then(categories => {
-      const categorieName = categories[i - 1].name;
-      bouton.id = categorieName;
-
-      bouton.addEventListener('click', function () {
-        
-      });
-    });
-    
-      formulaire.appendChild(bouton);
-    }
-  } else {
-    console.error("Formulaire non trouvé.");
-  }
+    .then(datas => {
+      console.log(datas);
+for ( let data of datas ) {
+  const bouton = document.createElement ('button');
+  bouton.innerText = data.name
+  bouton.id = data.name;
+  formulaire.appendChild(bouton);
 }
 
 
-creerBoutons();
+    })
+  }
 
-
-// function filtreObjet() {
-//      // Ajoutez un événement au bouton "Objet" pour déclencher la fonction de filtrage
-// const boutonObjet = document.querySelector(".btn-Objets");
-// if (boutonObjet) {
-//   boutonObjet.addEventListener("click", filtreObjet);
-// }
-//     // Remplacez ".gallery" par le sélecteur de votre galerie
-//   const gallery = document.querySelector(".gallery");
-//   const url = 'http://localhost:5678/api/works';
-
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(datas => {
-//       // Supprime tous les éléments enfants de la galerie actuelle
-//       while (gallery.firstChild) {
-//         gallery.removeChild(gallery.firstChild);
-//       }
-
-//         // Filtre données  "Objet"
-//       const objets = datas.filter(data => data.name === "Objet");
-
-//       // Création éléments HTML pour chaque objet et galerie
-//       objets.forEach(data => {
-//         const figure = document.createElement("figure");
-//         let dynamiqueimage = document.createElement("img");
-//         dynamiqueimage.src = data.imageUrl;
-//         dynamiqueimage.alt = data.title;
-//         const figcaption = document.createElement("figcaption");
-//         figcaption.innerHTML = data.title;
-
-//         figure.appendChild(dynamiqueimage);
-//         figure.appendChild(figcaption);
-//         gallery.appendChild(figure);
-    
-//       });
-     
-//     })
-    
-// }
-
-
-
-// filtreObjet();
-
-//innerHTML = ""
+AjoutBouton()
