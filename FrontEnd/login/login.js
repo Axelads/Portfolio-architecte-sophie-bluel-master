12caed3f4ec2a7d69fa205243a738441576d1aed
Axel Gregoire <professionnel.agregoire@gmail.com>
2023-11-27 13:39:40 +0100

    section login regler (msg d'erreur + redirection si correct )

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -3,43 +3,51 @@ function login() {
   const password = document.getElementById('password').value;
   const url = 'http://localhost:5678/api/users/login';
 
-
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-               },
-      body: JSON.stringify({
-        email: email,
-        password: password,
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
     })
-      
-    })
-       .then(response => response.json())
-       .then(data => {
-        localStorage.setItem("token", data.token);
-        window.location.href = '../index.html';
-        console.log(data);
-       })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erreur dans l’identifiant ou le mot de passe');
+    }
+    return response.json();
+  })
+  .then(data => {
+    localStorage.setItem('token', data.token);
+    window.location.href = '../index.html';
+    console.log(data);
+  })
+  .catch(error => {
+    const errorParagraph = document.createElement('p');
+    errorParagraph.textContent = error.message;
 
-       .catch ((error) => {
-        const errorParagraph = document.createElement('p');
-      errorParagraph.textContent = 'Mot de passe ou E-mail incorrect';
-      // Ajout l'ID "Error" à l'élément errorParagraph (qui est le <p>)
-      errorParagraph.id = 'Error';
-      
-      const h2 = document.getElementById('Log').querySelector('h2');
-      const existingError = document.getElementById('Error');
-        console.log(error);
-       });
- }
+    // Ajout l'ID "Error" à l'élément errorParagraph (qui est le <p>)
+    errorParagraph.id = 'Error';
 
- const loginForm = document.getElementById("LoginForm");
- loginForm.addEventListener("submit", (e) => {
+    const h2 = document.getElementById('Log').querySelector('h2');
+    const existingError = document.getElementById('Error');
+
+    // Si l'erreur n'est pas déjà affichée, l'ajouter
+    if (!existingError) {
+      h2.appendChild(errorParagraph);
+    }
+    console.error(error);
+  });
+}
+
+const loginForm = document.getElementById('LoginForm');
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   login();
- })
+});
 
 
 
