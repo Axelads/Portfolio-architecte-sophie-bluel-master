22d5b2ae9790c5a047b28eccd73d171dcd5a5c8f
Axel Gregoire <professionnel.agregoire@gmail.com>
2023-12-07 12:33:10 +0100

    nouveau .js , finition css de la modale AddPhoto , appel au fetch pour les categories

---
+++ FrontEnd/modaleAdd.js
@@ -0,0 +1,41 @@
+// Creation modale Ajout photo
+
+
+const addImgButton = document.querySelector('.addImg');
+const selectCategorie = document.getElementById('Selection-Categorie');
+
+
+addImgButton.addEventListener('click', function() {
+    const interieurModale = document.querySelector(".modal");
+
+      fetch('http://localhost:5678/api/categories')
+      .then(response => response.json())
+      .then(categories => {
+
+    interieurModale.innerHTML = `<div class="button-modale">
+               <button class="close-modal modal-trigger">X</button>
+               <button class="return-modal modal-trigger"><i class="fa-solid fa-arrow-left" style="color: #000000;"></i></button>
+        </div>
+        <h3>Ajout photo</h3>
+        <div class="ajoutPhoto">
+           <i class="fa-regular fa-image"></i>
+           <div class="AjouterPhoto">
+              <button>+ Ajouter photo</button>
+           </div>
+        <p>jpg, png : 4mo max</p>
+        </div>
+        <form id="AjoutInfos">
+                <label for="title">Titre</label>
+                <input type="text">
+                <label for="categorie">Catégorie</label>
+            <div class="select-wrapper">
+                <select id="Selection-Categorie" name="categorie" required>
+                     <option value="" disabled selected></option>
+                     ${categories.map(category => `<option value="${category.id}">${category.name}</option>`).join('')}
+                </select>
+            </div>
+                <div class="borderValider">
+                <input type="submit" value="Valider">
+                </div>`;
+})
+})
