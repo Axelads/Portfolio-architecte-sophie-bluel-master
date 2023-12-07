c7681418aae2a192fc70d484f444e1ab508742cc
Axel Gregoire <professionnel.agregoire@gmail.com>
2023-12-07 10:43:22 +0100

    preparation modale AjoutPhoto

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -10,7 +10,8 @@ document.addEventListener('DOMContentLoaded', PageAdmin);
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 const urlImage = 'http://localhost:5678/api/works';
-const modalGallery = document.querySelector(".modal-Galery")
+const modalGallery = document.querySelector(".modal-Galery");
+
 
 // creation de la modal
 
@@ -44,4 +45,42 @@ function toggleModal(){
 } else {
     modalGallery.innerHTML = "";
    }
-}
+};
+
+
+// Creation modale Ajout photo
+
+
+const addImgButton = document.querySelector('.addImg');
+
+
+
+addImgButton.addEventListener('click', function() {
+    const interieurModale = document.querySelector(".modal");
+    interieurModale.innerHTML = `<div class="button-modale">
+    <button class="close-modal modal-trigger">X</button>
+    <button class="return-modal modal-trigger"><i class="fa-solid fa-arrow-left" style="color: #000000;"></i></button>
+    </div>
+    <h3>Ajout photo</h3>
+    <div class="ajoutPhoto">
+    <i class="fa-regular fa-image"></i>
+    <div class="AjouterPhoto">
+    <button>+ Ajouter photo</button>
+    </div>
+    <p>jpg, png : 4mo max</p>
+    </div>
+    <form id="AjoutInfos">
+                <label for="title">Titre</label>
+                <input type="text">
+                <label for="categorie">Catégorie</label>
+            <div class="select-wrapper">
+                <select id="categorie" name="categorie" required>
+                     <option value="" disabled selected></option>
+                     <option value="categorie1">Catégorie 1</option>
+                     <option value="categorie2">Catégorie 2</option>
+                </select>
+            </div>
+                <div class="borderValider">
+                <input type="submit" value="Valider">
+                </div>`;
+});
